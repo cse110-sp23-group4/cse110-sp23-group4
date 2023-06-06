2f6155b3e3c9df2763bb7e857d41c845681dcd65
abijitj <abijit.jay@gmail.com>
2023-06-06 10:02:39 -0700

    Fixed test to check menu page category button functionality

--- source/__tests__/menu-page-test.js
+++ source/__tests__/menu-page-test.js
@@ -7,7 +7,6 @@ describe('Basic user flow for Menu Page', () => {
 
     beforeAll(async () => {
         console.log("Menu page tests starting...");
-        //await page.goto('http://127.0.0.1:8000/source/prototyping/menu-prototype.html');
     });
 
     beforeEach(async () => {
@@ -45,9 +44,7 @@ describe('Basic user flow for Menu Page', () => {
         await page.waitForNavigation();
 
         const page2URL = await page.url();
-        //console.log(page2URL);
         const page2Title = await page.title();
-        //console.log(page2Title);
 
         expect(page2Title).toBe('This is the prototype history page');
         expect(page2URL).toBe('http://127.0.0.1:8000/source/prototyping/saved-readings-prototype.html');
@@ -56,14 +53,11 @@ describe('Basic user flow for Menu Page', () => {
     test("Check if back button takes you back to landing page on click", async () => {
         console.log("Checking if back button takes you back to landing page on click...");
         const button = await page.$('#back');
-        //console.log(await(await button.getProperty('innerText')).jsonValue());
         await button.click();
         await page.waitForNavigation();
 
         const page2URL = await page.url();
-        //console.log(page2URL);
         const page2Title = await page.title();
-        //console.log(page2Title);
 
         expect(page2Title).toBe('The Fortune Hut');
         expect(page2URL).toBe('http://127.0.0.1:8000/source/prototyping/landing-prototype.html');
@@ -73,28 +67,18 @@ describe('Basic user flow for Menu Page', () => {
         console.log("Checking if hover animations work...");
         let buttons = await page.$$('button');
         let numButtons = buttons.length;
-        //console.log(buttons.length);
 
         prevBoxShadows = page.$$('button', el => {
             return getComputedStyle(el).getPropertyValue('box-shadow');
         });
 
         for(let i = 0; i < numButtons; i++){
-
-            //console.log("Before hover...");
-            //console.log(prevBoxShadow);
-
-            //const button = await page.$('button');
             await buttons[i].hover();
 
             const newBoxShadows = await page.$$('button', el => {
                 return getComputedStyle(el).getPropertyValue('box-shadow');
             });
 
-            //console.log("After hover...");
-            //console.log(newBoxShadow);
-
-            console.log(newBoxShadows[i]);
             expect(prevBoxShadows[i]).not.toBe(newBoxShadows[i]);
         }
     });
@@ -102,28 +86,23 @@ describe('Basic user flow for Menu Page', () => {
     test("Check if all the category buttons take you to the card reading page on click", async () => {
         console.log("Checking if all category buttons take you to the card reading page on click...");
 
-        const button = await page.$('.categoryButton');
         const buttons = await page.$$('.categoryButton');
+        const numButtons = buttons.length;
 
-        await button.click();
-        await page.waitForNavigation();
+        for(let i = 0; i < numButtons; i++){
+            const newButtons = await page.$$('.categoryButton');
 
-        const page2URL = await page.url();
-        //console.log(page2URL);
-        const page2Title = await page.title();
-        //console.log(page2Title);
-
-        for(let i = 0; i < buttons.length; i++){
-            await button.click();
+            await newButtons[i].click();
             await page.waitForNavigation();
 
             const page2URL = await page.url();
-            //console.log(page2URL);
             const page2Title = await page.title();
-            //console.log(page2Title);
 
-            expect(page2Title).toBe('The Fortune Hut');
+            expect(page2Title).toBe('Tarot Beta');
             expect(page2URL).toBe('http://127.0.0.1:8000/source/prototyping/card-prototype.html');
+
+            page.goto('http://127.0.0.1:8000/source/prototyping/menu-prototype.html'); 
+            await page.waitForNavigation();
         }
     });
 });
